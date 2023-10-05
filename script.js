dce8a1b570d2ef385cebf676cae45958c1df5f38
Ivan Peresvetov <lumenist@mail.ru>
2023-10-06 00:43:19 +0300

    var 2

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const width = window.innerWidth;
-//   if (width < 768) {
+(function () {
+  "use strict";
+  const breakpoint = window.matchMedia("(min-width:768px)");
+  let mySwiper;
+  const breakpointChecker = function () {
+    if (breakpoint.matches === true) {
+      if (mySwiper !== undefined) mySwiper.destroy(true, true);
+      return;
+    } else if (breakpoint.matches === false) {
+      return enableSwiper();
+    }
+  };
+  const enableSwiper = function () {
+    mySwiper = new Swiper(".swiper-container", {
+      loop: false,
+      slidesPerView: "auto",
+      centeredSlides: true,
+      a11y: true,
+      keyboardControl: true,
+      grabCursor: true,
+      pagination: ".swiper-pagination",
+      paginationClickable: true,
+    });
+  };
+  breakpoint.addListener(breakpointChecker);
+  breakpointChecker();
+})();
 
-//   }
-// });
+let expandButton = document.querySelector(".expand-button");
+let brandContainer = document.querySelector(".brand-list-container");
 
-if (window.matchMedia("(max-width: 768px)").matches) {
-  const swiper = new Swiper(".brand-list", {
-    // Optional parameters
-    direction: "horizontal",
-    loop: false,
+expandButton.addEventListener("click", function () {
+  if (expandButton.textContent == "Показать все") {
+    expandButton.classList.remove("expand-button--show");
+    expandButton.classList.add("expand-button--hide");
+    expandButton.textContent = "Скрыть";
 
-    // If we need pagination
-    pagination: {
-      el: ".swiper-pagination",
-    },
-
-    spaceBetween: 16,
-    slidesPerView: 1,
-  });
-}
-
-let expandButton = document.querySelector('.expand-button');
-let brandContainer = document.querySelector('.brand-list-container');
-
-expandButton.addEventListener('click', function () {
-  if (expandButton.textContent == 'Показать все') {
-    expandButton.classList.remove('expand-button--show');
-    expandButton.classList.add('expand-button--hide');
-    expandButton.textContent = 'Скрыть'
-
-    brandContainer.classList.remove('brand-list-container--minimazed')
-    brandContainer.classList.add('brand-list-container--expanded');
+    brandContainer.classList.remove("brand-list-container--minimazed");
+    brandContainer.classList.add("brand-list-container--expanded");
   } else {
-    expandButton.classList.remove('expand-button--hide');
-    expandButton.classList.add('expand-button--show');
-    expandButton.textContent = 'Показать все'
+    expandButton.classList.remove("expand-button--hide");
+    expandButton.classList.add("expand-button--show");
+    expandButton.textContent = "Показать все";
 
-    brandContainer.classList.remove('brand-list-container--expanded');
-    brandContainer.classList.add('brand-list-container--minimazed')
+    brandContainer.classList.remove("brand-list-container--expanded");
+    brandContainer.classList.add("brand-list-container--minimazed");
   }
-})
+});
